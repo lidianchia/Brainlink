69d4f10288e30564722f39a90833b37e42f29453
ittuann <ittuann@outlook.com>
2025-03-06 14:21:07 +0800

    init dist branch

--- src/pages/_app.jsx
+++
@@ -1,9 +0,0 @@
-import "@/styles/globals.css";
-import React, { Component } from "react";
-
-export default class App extends Component {
-  render() {
-    const { Component: PageComponent, pageProps } = this.props;
-    return <PageComponent {...pageProps} />;
-  }
-}
